95a4bdcc6df6d5dd2d71c741385aa1637402fdb7
David-PC\David <loliconck@gmail.com>
2017-07-30 00:32:12 +0800

    添加BaseEntity

---
+++ Framework.Orm.Dapper/Framework.Orm.Dapper.Core/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Framework.Orm.Dapper.Core
+{
+    public class BaseEntity
+    {
+        [Description("主键")]
+        public virtual Guid Id { get; set; }
+    }
+}
